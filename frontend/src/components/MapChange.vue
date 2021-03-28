487cbf5497c29749640c53384ca108b54eec0034
ryo.fukushima <ryo.fukushima@firstloop-tech.com>
2021-03-28 11:41:01 +0900

    fix

--- frontend/src/components/MapChange.vue
+++ frontend/src/components/MapChange.vue
@@ -105,7 +105,7 @@ export default {
         crime_type: this.$route.params['id'],
         // gradients: {0.4: 'blue', 0.65: 'lime', 1: 'red'}
         tileOptions: {
-          maxZoom: 15, 
+          maxZoom: 14, 
           minZoom: 5,
           minNativeZoom: 1,
           zoom:10, 
